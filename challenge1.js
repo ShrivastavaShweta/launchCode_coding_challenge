26c81b3affb129738732d9d29290bc3a4cd22f8f
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-04-13 13:47:48 -0500

    completed challenge 1

---
+++ challenge1.js
@@ -0,0 +1,8 @@
+    function areaOfTriangle(base, height){
+        let area = (base * height) / 2;
+        return area;
+    }
+
+    console.log(areaOfTriangle(3,2));
+    console.log(areaOfTriangle(7,4));
+    console.log(areaOfTriangle(10,10));
