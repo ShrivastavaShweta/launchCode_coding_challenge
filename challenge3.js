37f4ca3c0bdb89d3db8927a1c554e444a93a4373
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-04-28 22:26:06 -0500

    created a file for challenge 3

---
+++ challenge3.js
@@ -0,0 +1,12 @@
+//const input = require('readline-sync');
+
+function FirstReverse(str) { 
+
+    // code goes here
+      
+    return str; 
+  
+  }
+     
+  // keep this function call here 
+  console.log(FirstReverse(readline()));
