1034668eba95b9cd378fbd361d914e477fa89699
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-04-29 20:01:13 -0500

    completed challenge 3 but readline-sync not working on VScode

--- challenge3.js
+++ challenge3.js
@@ -1,12 +1,20 @@
-//const input = require('readline-sync');
+const input = require('readline-sync')
 
 function FirstReverse(str) { 
 
     // code goes here
-      
-    return str; 
+    let reverseStr = "";
+    let length = str.length;
+    for(let i=length-1; i>=0; i--){
+      console.log(str[i]);
+        reverseStr += str[i];
+    }  
+   console.log("reversed string " + reverseStr);
+    //str = reverseStr;
+    return reverseStr; 
   
   }
      
   // keep this function call here 
-  console.log(FirstReverse(readline()));
+  let inputStr = input.question("Enter in a string to reverse");
+  console.log(FirstReverse(inputStr));
